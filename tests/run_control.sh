416df53dfa9c8cbc98d4abf9f1ee49962068ea92
Matthew Jia <matthewjia@0587432740.wireless.umich.net>
2025-11-13 12:14:01 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

---
+++ tests/run_control.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# run_prefetch_test_no_flag.sh
+# Benchmark using perf without runtime toggle
+
+set -e
+
+BENCH=./test_control
+OUTDIR=results
+mkdir -p "$OUTDIR"
+
+echo "=== Running benchmark $BENCH"
+sudo taskset -c 2 perf stat -e \
+  dTLB-load-misses.walk_duration,\
+  dTLB-store-misses.walk_duration,\
+  cycles \
+  "$BENCH" 2>&1 | tee "$OUTDIR"/$BENCH
+
+echo "Results saved to $OUTDIR/$BENCH.txt"
+
+BENCH=./test_pt
+OUTDIR=results
+mkdir -p "$OUTDIR"
+
+echo "=== Running benchmark $BENCH"
+sudo taskset -c 2 perf stat -e \
+  dTLB-load-misses.walk_duration,\
+  dTLB-store-misses.walk_duration,\
+  cycles \
+  "$BENCH" 2>&1 | tee "$OUTDIR"/$BENCH
+
+echo "Results saved to $OUTDIR/$BENCH.txt"
+
+BENCH=./test_warm
+OUTDIR=results
+mkdir -p "$OUTDIR"
+
+echo "=== Running benchmark $BENCH"
+sudo taskset -c 2 perf stat -e \
+  dTLB-load-misses.walk_duration,\
+  dTLB-store-misses.walk_duration,\
+  cycles \
+  "$BENCH" 2>&1 | tee "$OUTDIR"/$BENCH
+
+echo "Results saved to $OUTDIR/$BENCH.txt"
