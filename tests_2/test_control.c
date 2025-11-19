e594c6ef5dca692aa49748974071b8bc6d516744
Matthew Jia <jiamatt@umich.edu>
2025-11-19 10:23:37 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- tests_2/test_control.c
+++ tests_2/test_control.c
@@ -244,7 +244,7 @@ int main(void) {
 		total_cyc_count += cyc;
 		printf("%zu,%" PRIu64 "\n", i, cyc);
 	}
-	fprintf(stderr, "acc=%u, total_cyc_count=%u\n", acc, ); // keep the loop "live"
+	fprintf(stderr, "acc=%u, total_cyc_count=%u\n", acc, total_cyc_count); // keep the loop "live"
 
 	// Cleanup
 	munlock(buf, len);
