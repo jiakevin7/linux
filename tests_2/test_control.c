420069545e5c8f0a4a96d6a21ab4a24258499f71
rainforestzhang <55517672+rainforestzhang@users.noreply.github.com>
2025-11-28 16:32:00 -0500

    cahged output a bit

--- tests_2/test_control.c
+++ tests_2/test_control.c
@@ -271,7 +271,7 @@ int main(void) {
 		total_cyc_count += cyc;
 	}
 
-	printf("test_pt cycles: %u\n", total_cyc_count);
+	printf("test_control cycles: %u\n", total_cyc_count);
 	// fprintf(stderr, "acc=%u, total_cyc_count=%u\n", acc, total_cyc_count);
 
 	// Cleanup
