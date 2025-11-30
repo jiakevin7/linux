ed511d3f9789146b6ddf53a5598b824fe8b369ae
Dajias1 <Dajias1@node0.dajias1-279101.umicheecs582-pg0.clemson.cloudlab.us>
2025-11-29 19:09:22 -0500

    moving to value entries

--- tests_2/test_warm.c
+++ tests_2/test_warm.c
@@ -254,7 +254,7 @@ int main(void) {
 		total_cyc_count += cyc;
 		// printf("%zu,%" PRIu64 "\n", i, cyc);
 	}
-	printf("test_warm cycles: %d", total_cyc_count);
+	printf("%u\n", total_cyc_count);
 	// fprintf(stderr, "acc=%u, total_cyc_count=%u\n", acc, total_cyc_count); // keep the loop "live"
 
 	// Cleanup
