75a74ed8b4fdb767638f851a04280f57d7ef046b
Matthew Jia <matthewjia@Matthews-MacBook-Pro-119.local>
2025-12-01 23:35:36 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- tests_2/test_control.c
+++ tests_2/test_control.c
@@ -249,9 +249,8 @@ int main(void) {
 		volatile char *cbuf = (volatile char *)regions[idx];
 
 		// data-dependent index to defeat hoisting and most HW prefetchers
-		size_t off = (acc * 1103515245u) & (REGION - 1);
 		uint64_t t0 = rdtscp_barrier();
-		acc += cbuf[off];
+		acc += cbuf[0];
 		uint64_t t1 = rdtscp_barrier();
 		uint64_t cyc = t1 - t0;
 		total_cyc_count += cyc;
