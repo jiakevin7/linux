695dce08f79ded7e2d2281cf51d2ab1ec481dec1
Matthew Jia <jiamatt@umich.edu>
2025-12-01 23:17:01 -0500

    moving to test branch

--- tests_2/test_100.sh
+++ tests_2/test_100.sh
@@ -6,19 +6,6 @@ OUTDIR=results0
 
 sum=0
 for i in {1..1000}; do
-	BENCH=./test_pt
-	mkdir -p "$OUTDIR"
-
-	  "$BENCH" 2>&1 | tee "$OUTDIR"/$BENCH
-
-	cat "$OUTDIR"/$BENCH >> combined.txt
-	value=$(cat "$OUTDIR/$BENCH")
-	sum=$(( sum + value ))
-
-done
-echo "total cycles for pt: $sum" >> combined.txt
-sum=0
-for i in {1..1000}; do
 	BENCH=./test_control
 	mkdir -p "$OUTDIR"
 
@@ -30,6 +17,21 @@ for i in {1..1000}; do
 	
 done
 echo "total cycles for control: $sum" >> combined.txt
+
+sum=0
+for i in {1..1000}; do
+	BENCH=./test_pt
+	mkdir -p "$OUTDIR"
+
+	  "$BENCH" 2>&1 | tee "$OUTDIR"/$BENCH
+
+	cat "$OUTDIR"/$BENCH >> combined.txt
+	value=$(cat "$OUTDIR/$BENCH")
+	sum=$(( sum + value ))
+
+done
+echo "total cycles for pt: $sum" >> combined.txt
+
 sum=0
 for i in {1..1000}; do
 	BENCH=./test_warm
