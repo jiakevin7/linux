3a1890821fa1d8dc3be79dde9ecb9cb236e07857
Matthew Jia <jiamatt@umich.edu>
2025-12-04 01:06:23 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- end-to-end tests/btree/src/main.c
+++ end-to-end tests/btree/src/main.c
@@ -194,11 +194,18 @@ int main(int argc, char *argv[])
     fprintf(opt_file_out, "</run>\n");
 
     gettimeofday(&tend, NULL);
-    double total =
-    (double)(tend.tv_sec - tstart.tv_sec) +
-    (double)(tend.tv_usec - tstart.tv_usec) / 1e6;
 
-    printf("Total time: %.6f seconds\n", total);
+    long sec  = tend.tv_sec  - tstart.tv_sec;
+    long usec = tend.tv_usec - tstart.tv_usec;
+
+    if (usec < 0) {
+            sec  -= 1;
+            usec += 1000000;  // borrow 1 second = 1,000,000 usec
+    }
+
+    double elapsed = (double)sec + (double)usec / 1e6;
+
+    printf("%.6f\n", elapsed);
 
     fprintf(opt_file_out, "</benchmark>\n");
     return 0;
